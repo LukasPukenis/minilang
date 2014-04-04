46b7fb4b27add6580b22ad0d8b4514e96861aa44
Lukas Pukenis <lukas.pukenis@gmail.com>
2014-04-04 20:08:03 +0300

    Cleaned up a bit, introduced .gitignore because of Visual Studio project mess

--- Source.c
+++ Source.c
@@ -3,19 +3,29 @@
 
 #define NULL 0
 
-union node_value {
-	int iValue;
-	float fValue;
-};
+typedef enum {
+	VALUE,
+	OPERATOR
+} NODE_TYPE;
+
+typedef enum {
+	ADD,
+	SUB,
+	MUL,
+	DIV
+} OPERATOR_TYPE;
 
 struct ast {
-	enum { VALUE, OPERATOR } node_type;
-	enum { INT, FLOAT} value_type;
-	enum { ADD, SUB, MUL, DIV} operator_type;
+	NODE_TYPE node_type;
+	OPERATOR_TYPE operator;
 	short precedence;
-	union node_value value;
+	double value;
 	struct ast* next;
-};
+	bool reduced;
+	struct ast * left;
+	struct ast * right;
+	struct ast * parent;
+} ast_head, syntax_tree_head;
 
 void print_ast(struct ast * head) {
 	printf("\nPrinting AST: ");
@@ -26,54 +36,122 @@ void print_ast(struct ast * head) {
 	curr = curr->next;
 
 	while (curr != NULL) {
-		if (curr->value_type == INT) {
-			printf("Node INT %d", curr->value.iValue);
+		if (curr->node_type == VALUE) {
+			printf("Node INT %f", curr->value);
 		}
 		else {
-			printf("Node FLOAT %f", curr->value.fValue);
+			printf("Node OPERATOR %f", curr->value);
 		}
 		printf(" | ");
-
 		curr = curr->next;
 	}
 }
 
-char input[] = "123+456.78";
+void syntax_tree_print(struct ast * head) {
+
+	printf("\n\nPrinting Syntax Tree: ");
+
+	printf("\nDone printing Syntax Tree\n");
+}
+
+void syntax_tree_insert(struct ast * instead, struct ast * combined) {
+
+}
+
+void syntax_tree_build(struct ast * head) {
+	printf("\n\nBuilding syntax tree will\n");
+
+	printf("\nDone building\n");
+}
+
+void ast_add_value(double value) {
+	struct ast * new_node = (struct ast*)malloc(sizeof(struct ast));
+	new_node->node_type = VALUE;
+	new_node->next = NULL;
+	new_node->reduced = false;
+
+	new_node->value = value;
+	struct ast * curr = &ast_head;
+
+	// rewing head to the last element
+	while (curr->next != NULL) {
+		curr = curr->next;
+	}
+
+	curr->next = new_node;
+}
+
+void ast_add_operator(OPERATOR_TYPE type) {
+	struct ast * new_node = (struct ast*)malloc(sizeof(struct ast));
+	new_node->node_type = OPERATOR;
+	new_node->next = NULL;
+	new_node->reduced = false;
+
+	new_node->operator = type;
+	struct ast * curr = &ast_head;
+
+	// rewing head to the last element
+	while (curr->next != NULL) {
+		curr = curr->next;
+	}
+
+	curr->next = new_node;
+}
+
+char input[] = "100-123+456.78";
 
 int main(int argc, char** argv)
 {
 	printf("Lets process this: %s\n", input);
 
-	// head and last elements of ast to amortize the inserting
-	struct ast * ast_head = (struct ast*)malloc(sizeof(struct ast));
-	ast_head->next = NULL;
+	ast_head.next = NULL;
 
 	// This will be set once numeric is detected. Then if dot is detected it will be known it's float
 	int numeric_start_pos = -1;
-	union node_value numeric;
 	bool last_was_numeric = false;
 	bool dot_was_detected = false;
 
-	for (int i = 0, len = sizeof(input) - 1; i < len; i++) {
-		printf("\nProcessing %c\n", input[i]);
-		char val = input[i];		
+	for (int i = 0, len = sizeof(input)-1; i < len; i++) {
+		printf("\n*************\nProcessing %c\n", input[i]);
+		char val = input[i];
+
+		// check for numeric token end
+		if (i + 1 == len || ((val < '0' || val > '9') && val != '.' && numeric_start_pos != -1 && last_was_numeric)) {
+			printf("Numeric ended at %d\n", i);
+
+			char * buffer = malloc((i - numeric_start_pos) *sizeof(char));
+			strncpy(buffer, (input + numeric_start_pos), i - numeric_start_pos + 1);
+
+			ast_add_value(atof(buffer));
+
+			printf("Numeric translated: %f", atof(buffer));
+
+			dot_was_detected = false;
+			numeric_start_pos = -1;
+			last_was_numeric = false;
+		}
 
 		if (val >= '0' && val <= '9') {
 			printf("Numeric detected at %d. Not sure if int or float yet\n", i);
 			if (numeric_start_pos == -1 && !last_was_numeric) {
 				numeric_start_pos = i;
 				last_was_numeric = true;
-			}			
-
-		} else if (val == '+' || val == '-') {
-			printf("+ or - detected\n");
-
+			}
 
 		}
-		else if (val == '*' && val == '/') {
-			printf("* or / detected\n");
-
+		else if (val == '+') {
+			ast_add_operator(ADD);
 		}
+		else if (val == '-') {
+			ast_add_operator(SUB);
+		}
+		else if (val == '*') {
+			ast_add_operator(MUL);
+		}
+		else if (val == '/') {
+			ast_add_operator(DIV);
+		}
+
 		else if (val == '.') {
 			dot_was_detected = true;
 		}
@@ -82,51 +160,14 @@ int main(int argc, char** argv)
 
 		}
 
-		// check for numeric token end
-		if ( i+1 == len || ((val < '0' || val > '9') && val != '.' && numeric_start_pos != -1 && last_was_numeric)) {
-			printf("Numeric ended at %d\n", i);
-
-			char buffer[100];
-			memset(buffer, '\0', sizeof(buffer));
-			strncpy(buffer, (input + numeric_start_pos), i - numeric_start_pos+1);
-			struct ast * new_node = (struct ast*)malloc(sizeof(struct ast));
-			new_node->node_type = VALUE;
-			new_node->next = NULL;
-
-			if (dot_was_detected) {
-				new_node->value.fValue = atoi(buffer);
-				new_node->value_type = FLOAT;
-				struct ast * curr = ast_head;
-
-				// rewing head to the last element
-				while (curr->next != NULL) {
-					curr = curr->next;
-				}
-				curr->next = new_node;
-
-				printf("Float translated: %f", atof(buffer));
-			}
-			else {				
-				new_node->value.iValue = atoi(buffer);
-				new_node->value_type = INT;
-				struct ast * curr = ast_head;
-				
-				// rewing head to the last element
-				while (curr->next != NULL) {					
-					curr = curr->next;
-				}
-				curr->next = new_node;
-				
-				printf("Int translated: %d", atoi(buffer));
-			}			
-
-			dot_was_detected = false;
-			numeric_start_pos = -1;
-			last_was_numeric = false;
-		}
 	}
 
-	print_ast(ast_head);
+	print_ast(&ast_head);
+
+	syntax_tree_build(&ast_head);
+
+	syntax_tree_print(&syntax_tree_head);
+
 	getch();
 	return 0;
 }
